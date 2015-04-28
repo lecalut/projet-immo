2eeef79630b25266017ef8268b25f410d40f63d9
Damien <yorlin@hotmail.fr>
2015-04-29 00:09:17 +0200

    jhb

--- Contact.php
+++ Contact.php
@@ -1,8 +1,18 @@
 <?php
 session_start();
 include("BDD.php");
-include("header.php");
 ?>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="Agence.css"/>
+    <link rel="icon" type="image/jpg" href="favicon.jpg" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700">
+      <meta charset="utf-8">
+
+  <title> Immo Lyon </title>
+  </head>
+<body>
 <p id="Contact">Nous Contacter</p>
 
 <iframe id="map"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44530.67626440062!2d4.88390433828127!3d45.767837984111125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea8ef11e830d%3A0xfeef839e1645ec3c!2s2+Rue+Curie%2C+69006+Lyon!5e0!3m2!1sfr!2sfr!4v1427399601393" width="1000" height="650" frameborder="0" style="border:0"></iframe>    <form method="get" action="">
@@ -22,6 +32,13 @@ include("header.php");
 <li><p>Immo Meyzieu <br> 1 Rue des yeux <br> 69330 Meyzieu <br> 06 41 11 25 44</p></li>
   </ol>
   <a id="Accueil" href="index.php"><i class="fa fa-arrow-circle-left"></i></a>
+
+
+
+
+  
+  </UL>
+</nav>
 
   </body>
 </html>
--- header.php
+++ header.php
@@ -5,8 +5,13 @@
  		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
  		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700">
    		<meta charset="utf-8">
-
- 	<title> Immo Lyon </title>
+  		<meta charset='utf-8'>
+   		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+   		<link rel="stylesheet" href="styles.css">
+   		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   		<script src="script.js"></script>
+ 		<title> Immo Lyon </title>
 	</head>
 <body>
 <img class="logo" src="Immologo.png">
