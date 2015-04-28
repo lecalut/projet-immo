d1bf5c1dfc0dddc0cd21fecc6f5595b3b6c0c5c6
lecalut <lecalut@hotmail.fr>
2015-04-28 16:04:41 +0200

    j

--- achat.php
+++ achat.php
@@ -9,7 +9,7 @@ include("BDD.php");
 
 <?php
 
-$sql = "SELECT type_bien, image, date, cp_bien, ville_bien, surface_bien, prix_bien, type_chauff_bien, description_bien, type_vente_bien FROM biens ORDER BY date DESC";
+$sql = "SELECT * FROM biens  ORDER BY date DESC";
 $req = $bdd->prepare($sql);
 $req->execute(array(
 ));
@@ -19,7 +19,7 @@ while ($donne = $req->fetch() )
 
 <div><img src="image/<?php echo $donne['image']; ?>" />
 
-<?php 
+ <?php
 echo $donne['type_bien'];
 echo $donne['type_vente_bien'];
 echo $donne['date'];
@@ -29,7 +29,6 @@ echo $donne['surface_bien'];
 echo $donne['prix_bien'];
 echo $donne['type_chauff_bien'];
 echo $donne['description_bien'];
-
 
 } 
 
