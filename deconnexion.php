800f3d4b5b500eae107dac4a44fd4c3a4845a707
Damien <yorlin@hotmail.fr>
2015-04-29 10:42:56 +0200

    Bonjour

--- deconnexion.php
+++ deconnexion.php
@@ -1,14 +1,26 @@
 <?php
 session_start();
-
-
+include("BDD.php");
 ?>
-<div class="validation">
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="Agence.css"/>
+    <link rel="icon" type="image/jpg" href="favicon.jpg" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700">
+      <meta charset="utf-8">
+
+  <title> Immo Lyon </title>
+  </head>
+<div class="inscription">
 
 
 <?php
     $_SESSION['pseudo'] = 0;
     $_SESSION['id'] = 0;
+
+    echo "Vous avez &eacutet&eacute d&eacuteconnect&eacute !";
+
 ?>   
 
     
@@ -16,5 +28,5 @@ session_start();
 
 </div>
 <?php
-header ("Refresh: 0;URL=index.php");
+header ("Refresh: 2;URL=index.php");
 ?>
--- validation_connexion.php
+++ validation_connexion.php
@@ -2,6 +2,16 @@
 session_start();
 include("BDD.php");
 ?>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="Agence.css"/>
+    <link rel="icon" type="image/jpg" href="favicon.jpg" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700">
+      <meta charset="utf-8">
+
+  <title> Immo Lyon </title>
+  </head>
 <div class="inscription">
 <?php
 $pseudo = $_POST['pseudo'];
@@ -31,7 +41,7 @@ else
     $_SESSION['id'] = $resultat['id'];
     $_SESSION['pseudo'] = $pseudo;
    
-echo "You are connected !";
+echo "Vous &ecirctes connect&eacute !";
 
 
 }
@@ -44,5 +54,5 @@ $req->closeCursor();
 
 
 <?php
-header ("Refresh: 0;URL=index.php");
+header ("Refresh: 2;URL=index.php");
 ?>
